df0a9362b261983e4e5774aa6b5a208416eb0276
Pepich <benedikt.abel@yahoo.de>
2016-06-05 23:55:22 +0200

    Added headers

--- B-12/produkte.c
+++ B-12/produkte.c
@@ -1,1 +1,14 @@
-#
+/********************************/
+/* Benedikt Abel, Justus Faust  */
+/* Gruppe: Di08                 */
+/* Uebungsblatt: 6, Aufgabe B-12 */
+/* Thema: produkte              */
+/* Version: v1.0.0              */
+/* Datum: 20160605              */
+/* Status: WIP                  */
+/********************************/
+
+int main(void)
+{
+	
+}
--- B-13/pythagorae.c
+++ B-13/pythagorae.c
@@ -1,1 +1,14 @@
-#
+/********************************/
+/* Benedikt Abel, Justus Faust  */
+/* Gruppe: Di08                 */
+/* Uebungsblatt: 6, Aufgabe B-13 */
+/* Thema: pythagorae            */
+/* Version: v1.0.0              */
+/* Datum: 20160605              */
+/* Status: WIP                  */
+/********************************/
+
+int main(void)
+{
+	
+}
