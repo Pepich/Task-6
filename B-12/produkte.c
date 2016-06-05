be8cc1b8ba45c24c7d71d8ef380499d8a5cfbd10
Pepich <benedikt.abel@yahoo.de>
2016-06-06 00:07:02 +0200

    Finished task B-12

--- B-12/produkte.c
+++ B-12/produkte.c
@@ -8,7 +8,28 @@
 /* Status: WIP                  */
 /********************************/
 
+#include <stdio.h>
+
 int main(void)
 {
-	
+    int zahl = 0;
+    while (1)
+    {
+	printf("Bitte Zahl eingeben (negativ = ende): ");
+	scanf("%i", &zahl);
+	if (zahl < 0)
+	    break;
+        int prim = 1;
+	for (int i = 1; i <= zahl; i++)
+	{
+	    if (zahl % i == 0)
+	    {
+		printf("%i * %i\n", i, zahl/i);
+		if (i != 1 && i != zahl)
+		    prim = 0;
+	    }
+	}
+	printf("%i ist %ceine Primzahl!\n\n", zahl, prim ? '\0' : 'k');
+    }
+    return 0;
 }
